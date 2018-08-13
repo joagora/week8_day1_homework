9f5652c7136d4e3e747f076256a7bb08adf27ded
Joanna Gora <joanna.gora@protonmail.com>
2018-08-13 21:19:04 +0100

    created setup for Hibernate

---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+import models.Book;
+import models.Writer;
+
+public class Runner {
+    public static void main(String[] args) {
+        Book book = new Book("Everything is Illuminated", 120);
+        Writer writer = new Writer("Jonathan", "Safran Foer");
+    }
+}
